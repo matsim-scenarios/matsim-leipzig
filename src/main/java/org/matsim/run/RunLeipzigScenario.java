472ab5b711a53e727751cb21c8e6659257786012
GregorRyb <rybczak@vsp.tu-berlin.de>
2023-12-05 16:35:41 +0100

    change version number

--- src/main/java/org/matsim/run/RunLeipzigScenario.java
+++ src/main/java/org/matsim/run/RunLeipzigScenario.java
@@ -74,7 +74,7 @@ public class RunLeipzigScenario extends MATSimApplication {
 	/**
 	 * Current version number.
 	 */
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.3";
 	private static final Logger log = LogManager.getLogger(RunLeipzigScenario.class);
 	@CommandLine.Mixin
 	private final SampleOptions sample = new SampleOptions(1, 10, 25);
