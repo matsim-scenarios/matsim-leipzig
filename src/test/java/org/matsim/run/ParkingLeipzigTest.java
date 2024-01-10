3bafc817c166b0946d35f4846981d3a2e7e6002e
GregorRyb <rybczak@vsp.tu-berlin.de>
2024-01-10 16:15:36 +0100

    sketch first test for parking and car free zones

--- src/test/java/org/matsim/run/ParkingLeipzigTest.java
+++ src/test/java/org/matsim/run/ParkingLeipzigTest.java
@@ -1,16 +1,31 @@
 package org.matsim.run;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.matsim.analysis.ParkingLocation;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.events.ActivityStartEvent;
+import org.matsim.api.core.v01.events.PersonMoneyEvent;
+import org.matsim.api.core.v01.events.handler.ActivityStartEventHandler;
+import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.api.core.v01.population.Population;
 import org.matsim.application.MATSimApplication;
+import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.events.EventsManagerImpl;
+import org.matsim.core.events.EventsUtils;
 import org.matsim.core.population.PopulationUtils;
+import org.matsim.run.prepare.NetworkOptions;
 import org.matsim.simwrapper.SimWrapperConfigGroup;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
@@ -21,9 +36,9 @@ public class ParkingLeipzigTest {
 	private static final String exampleShp = "input/v1.2/drtServiceArea/Leipzig_stadt.shp";
 
 	@Test
-	public final void runPoint1pctIntegrationTest() {
+	public final void runPoint1pctIntegrationTestWithParking() {
 		Path output = Path.of("output-parking-test/it-1pct");
-		Config config = ConfigUtils.loadConfig("input/v1.2/leipzig-v1.2-25pct.config.xml");
+		Config config = ConfigUtils.loadConfig("input/v1.2/leipzig-v1.2-10pct.config.xml");
 		config.global().setNumberOfThreads(1);
 		config.qsim().setNumberOfThreads(1);
 		config.controler().setLastIteration(0);
@@ -36,9 +51,50 @@ public class ParkingLeipzigTest {
 				"--parking-cost-area", "input/v" + "1.2" + "/parkingCostArea/Bewohnerparken_2020.shp",
 				"--intermodality", "drtAsAccessEgressForPt", "--parking" );
 
-		assertThat(output)
-				.exists()
-				.isNotEmptyDirectory();
-		new ParkingLocation().execute("--directory", output.toString());
+		//new ParkingLocation().execute("--directory", output.toString());
+
+		EventsManager eventsManager = EventsUtils.createEventsManager();
+		eventsManager.addHandler(new ParkingActivityStartEventHandler());
+		EventsUtils.readEvents(eventsManager , output +"/" + "/leipzig-1pct.output_events.xml.gz");
+		Assert.assertTrue(ParkingActivityStartEventHandler.parkingEvents.size() >0);
+		for (ActivityStartEvent event: ParkingActivityStartEventHandler.parkingEvents) {
+			if (event.getPersonId().equals("221462")) {
+				Assert.assertTrue(event.getLinkId().equals("-152600315#3"));
+			}
+		}
 	}
+
+	@Test
+	public final void runPoint1pctIntegrationTestWithParkingWithCarFreeArea() {
+		Path output = Path.of("output-parking-test-withCarFreeArea/it-1pct");
+		Config config = ConfigUtils.loadConfig("input/v1.2/leipzig-v1.2-10pct.config.xml");
+		config.global().setNumberOfThreads(1);
+		config.qsim().setNumberOfThreads(1);
+		config.controler().setLastIteration(0);
+		config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
+		config.controler().setOutputDirectory(output.toString());
+		ConfigUtils.addOrGetModule(config, SimWrapperConfigGroup.class).defaultDashboards = SimWrapperConfigGroup.Mode.disabled;
+		config.plans().setInputFile(URL + "leipzig-v1.2-0.1pct.plans-initial.xml.gz");
+
+		MATSimApplication.execute(RunLeipzigScenario.class, config, "run", "--1pct","--drt-area", exampleShp, "--post-processing", "disabled",
+				"--parking-cost-area", "input/v" + "1.2" + "/parkingCostArea/Bewohnerparken_2020.shp",
+				"--intermodality", "drtAsAccessEgressForPt", "--parking", "--car-free-area", exampleShp );
+
+		EventsManager eventsManager = EventsUtils.createEventsManager();
+		eventsManager.addHandler(new ParkingActivityStartEventHandler());
+		EventsUtils.readEvents(eventsManager , output +"/" + "/leipzig-1pct.output_events.xml.gz");
+		Assert.assertTrue(ParkingActivityStartEventHandler.parkingEvents.size() == 0);
+	}
+
+	class ParkingActivityStartEventHandler implements ActivityStartEventHandler {
+		static List<ActivityStartEvent> parkingEvents = new ArrayList<>();
+		@Override
+		public void handleEvent(ActivityStartEvent activityStartEvent) {
+			if (activityStartEvent.getActType().equals("parking interaction")) {
+				parkingEvents.add(activityStartEvent);
+			}
+		}
+	}
+
 }
+
