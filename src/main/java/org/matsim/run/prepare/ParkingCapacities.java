3ab9085b8139f3a3f1cb5f976ced4a95724d0fc7
GregorRyb <rybczak@vsp.tu-berlin.de>
2024-01-24 14:45:57 +0100

    satisfy checkstyle

--- src/main/java/org/matsim/run/prepare/ParkingCapacities.java
+++ src/main/java/org/matsim/run/prepare/ParkingCapacities.java
@@ -1,31 +1,32 @@
 package org.matsim.run.prepare;
 
-import org.matsim.analysis.ParkingLocation;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
-import org.matsim.application.MATSimAppCommand;
 import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.utils.io.IOUtils;
-import picocli.CommandLine;
-
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+/**
+ * This class writes out a network and a tsv file with the parking capacites of Leipzig according to a simplified calculation based on the RASt.
+ *
+ *
+ */
 
-public class ParkingCapacities {
+public final class ParkingCapacities {
+
+	private static Network network = NetworkUtils.readNetwork("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/leipzig/leipzig-v1.2/input/leipzig-v1.2-network-with-pt.xml.gz");
+	private static List<ParkingCapacities.ParkingCapacityRecord> listOfParkingCapacities = new ArrayList<>();
+	private ParkingCapacities() {
+		throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+	}
+
 
 
 	public static void main(String[] args) throws IOException {
-
-		Network network = NetworkUtils.readNetwork("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/leipzig/leipzig-v1.2/input/leipzig-v1.2-network-with-pt.xml.gz");
-		List<ParkingCapacities.ParkingCapacityRecord> listOfParkingCapacities = new ArrayList<>();
-
 		for (Link l: network.getLinks().values()) {
 
 			//skip motorways and non car links
@@ -52,14 +53,14 @@ public class ParkingCapacities {
 		BufferedWriter writer = IOUtils.getBufferedWriter(outputFolder.resolve("parkingCapacities.tsv").toString());
 		writer.write("linkId\tmaxCapacity\tminCapacity");
 		writer.newLine();
-
 		for (ParkingCapacities.ParkingCapacityRecord pd : listOfParkingCapacities) {
 			writer.write(pd.linkId + "\t" + pd.maxCapacity + "\t" + pd.minCapacity);
 			writer.newLine();
 		}
 		writer.close();
-
 	}
 
 	 private record ParkingCapacityRecord(String linkId, int maxCapacity, int minCapacity) { }
+
+
 }
