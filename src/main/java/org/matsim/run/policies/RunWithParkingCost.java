0c274b637053a4156d182b4c5b18736a09a9132b
GregorRyb <rybczak@vsp.tu-berlin.de>
2024-03-14 20:11:55 +0100

    implement parking case run class

---
+++ src/main/java/org/matsim/run/policies/RunWithParkingCost.java
@@ -0,0 +1,23 @@
+package org.matsim.run.policies;
+
+import org.matsim.application.MATSimApplication;
+import org.matsim.run.LeipzigScenario;
+
+/**
+ * Main class to run the Leipzig with parking cost scenario.
+ */
+public class RunWithParkingCost {
+
+	private RunWithParkingCost() {
+	}
+
+	public static void main(String[] args) {
+		String[] argsForPolicy = new String[]{
+				"--config", "/net/ils/matsim-leipzig/input/v1.3/leipzig-v1.3.1-10pct.config.xml",
+				"--parking",
+				"--parking-cost-area", "/net/ils/matsim-leipzig/input/v1.3/parkingCostArea/Bewohnerparken_2020.shp"
+		};
+		MATSimApplication.runWithDefaults(LeipzigScenario.class, argsForPolicy);
+	}
+
+}
