1a46a742344d84e23d405f6b37bf65a54ff2f3da
simei94 <67737999+simei94@users.noreply.github.com>
2025-04-14 17:11:47 +0200

    Cycle highways analysis (#53)
    * add cycle highway dashboard + analysis WIP
    
    * analysis still WIP
    
    * finalize dashboard + analysis
    
    * analysis for mean distances per mode
    
    * add mode share viz analyis
    
    * checkstyle
    
    * update to upload-artifact v4

---
+++ src/main/java/org/matsim/analysis/CycleHighwayAnalysis.java
@@ -0,0 +1,454 @@
+package org.matsim.analysis;
+
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntList;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.lang3.Range;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.events.LinkEnterEvent;
+import org.matsim.api.core.v01.events.PersonEntersVehicleEvent;
+import org.matsim.api.core.v01.events.handler.LinkEnterEventHandler;
+import org.matsim.api.core.v01.events.handler.PersonEntersVehicleEventHandler;
+import org.matsim.application.CommandSpec;
+import org.matsim.application.MATSimAppCommand;
+import org.matsim.application.options.CsvOptions;
+import org.matsim.application.options.InputOptions;
+import org.matsim.application.options.OutputOptions;
+import org.matsim.application.options.ShpOptions;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.events.EventsUtils;
+import org.matsim.core.events.MatsimEventsReader;
+import org.matsim.core.utils.gis.ShapeFileWriter;
+import org.matsim.core.utils.io.IOUtils;
+import org.matsim.vehicles.Vehicle;
+import picocli.CommandLine;
+import tech.tablesaw.api.*;
+import tech.tablesaw.io.csv.CsvReadOptions;
+import tech.tablesaw.joining.DataFrameJoiner;
+import tech.tablesaw.selection.Selection;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.*;
+
+import static org.matsim.application.ApplicationUtils.globFile;
+import static tech.tablesaw.aggregate.AggregateFunctions.count;
+
+@CommandLine.Command(name = "cycle-highway", description = "Calculates various cycle highway related metrics.")
+@CommandSpec(
+	requireRunDirectory=true,
+	produces = {"mode_share.csv", "mode_share_base.csv", "mode_shift.csv", "bike_income_groups.csv", "bike_income_groups_base.csv", "allModes_income_groups_base_leipzig.csv",
+		"bike_income_groups_base_leipzig.csv", "car_income_groups_base_leipzig.csv", "walk_income_groups_base_leipzig.csv", "ride_income_groups_base_leipzig.csv",
+		"pt_income_groups_base_leipzig.csv", "cycle_highway_agents_trip_start_end.csv", "mean_travel_stats.csv", "cycle_highways.shp"}
+)
+public class CycleHighwayAnalysis implements MATSimAppCommand {
+	private static final Logger log = LogManager.getLogger(CycleHighwayAnalysis.class);
+
+	@CommandLine.Mixin
+	private InputOptions input = InputOptions.ofCommand(CycleHighwayAnalysis.class);
+	@CommandLine.Mixin
+	private OutputOptions output = OutputOptions.ofCommand(CycleHighwayAnalysis.class);
+	@CommandLine.Option(names = "--base-path", description = "Path to run directory of base case.", required = true)
+	private Path basePath;
+	@CommandLine.Mixin
+	private ShpOptions shp;
+	@CommandLine.Option(names = "--highways-shp-path", description = "Path to run directory of base case.", required = true)
+	private String highwaysShpPath;
+	@CommandLine.Option(names = "--crs", description = "CRS for shp files.", defaultValue = "EPSG:25832")
+	private String crs;
+	@CommandLine.Option(names = "--dist-groups", split = ",", description = "List of distances for binning", defaultValue = "0.,1000.,2000.,5000.,10000.,20000.")
+	private List<Double> distGroups;
+	@CommandLine.Option(names = "--income-groups", split = ",", description = "List of income for binning. Derived from SrV 2018.", defaultValue = "0.,500.,900.,1500.,2000.,2600.,3000.,3600.,4600.,5600.")
+	private List<Double> incomeGroups;
+	@CommandLine.Option(names = "--modes", split = ",", description = "List of transport modes for mode share and switch.", defaultValue = "car,bike,ride,walk,pt")
+	private Set<String> modes;
+
+	List<String> modeOrder = null;
+//	cannot use the original String from class CreateBicycleHighwayNetwork because the class is on another branch. In the matsim version of this branch Simwrapper was not yet implemented
+	private static final String LINK_PREFIX = "cycle-highway-";
+	private static final String INCOME_SUFFIX = "_income_groups_base_leipzig.csv";
+	private static final String PERSON = "person";
+	private static final String INCOME = "income";
+	private static final String TRAV_TIME = "trav_time";
+	private static final String TRAV_DIST = "traveled_distance";
+	private static final String MAIN_MODE = "main_mode";
+	private static final String LONG_MODE = "longest_distance_mode";
+	private static final String TRIP_ID = "trip_id";
+	private final Map<Id<Vehicle>, String> bikers = new HashMap<>();
+	private final Map<String, List<Integer>> highwayPersons = new HashMap<>();
+
+	public static void main(String[] args) {
+		new CycleHighwayAnalysis().execute(args);
+	}
+
+	@Override
+	public Integer call() throws Exception {
+
+		//		all necessary file input paths are defined here
+		String eventsPath = globFile(input.getRunDirectory(), "*output_events.xml.gz").toString();
+		String personsPath = globFile(input.getRunDirectory(), "*output_persons.csv.gz").toString();
+		String tripsPath = globFile(input.getRunDirectory(), "*output_trips.csv.gz").toString();
+		String basePersonsPath = globFile(basePath, "*output_persons.csv.gz").toString();
+		String baseTripsPath = globFile(basePath, "*output_trips.csv.gz").toString();
+
+		EventsManager manager = EventsUtils.createEventsManager();
+		manager.addHandler(new CycleHighwayEventHandler());
+		manager.initProcessing();
+
+		MatsimEventsReader reader = new MatsimEventsReader(manager);
+		reader.readFile(eventsPath);
+		manager.finishProcessing();
+
+//		read necessary tables
+		Table persons = Table.read().csv(CsvReadOptions.builder(IOUtils.getBufferedReader(personsPath))
+			.columnTypesPartial(Map.of(PERSON, ColumnType.TEXT, INCOME, ColumnType.DOUBLE, "subpopulation", ColumnType.TEXT))
+			.sample(false)
+			.separator(CsvOptions.detectDelimiter(personsPath)).build());
+
+		Map<String, ColumnType> columnTypes = new HashMap<>(Map.of(PERSON, ColumnType.TEXT,
+			TRAV_TIME, ColumnType.STRING, "dep_time", ColumnType.STRING,
+			LONG_MODE, ColumnType.STRING, MAIN_MODE, ColumnType.STRING, TRAV_DIST, ColumnType.DOUBLE,
+			"first_act_x", ColumnType.DOUBLE, "first_act_y", ColumnType.DOUBLE, TRIP_ID, ColumnType.TEXT));
+
+		Table trips = Table.read().csv(CsvReadOptions.builder(IOUtils.getBufferedReader(tripsPath))
+			.columnTypesPartial(columnTypes)
+			.sample(false)
+			.separator(CsvOptions.detectDelimiter(tripsPath)).build());
+
+		Table basePersons = Table.read().csv(CsvReadOptions.builder(IOUtils.getBufferedReader(basePersonsPath))
+			.columnTypesPartial(Map.of(PERSON, ColumnType.TEXT, INCOME, ColumnType.DOUBLE))
+			.sample(false)
+			.separator(CsvOptions.detectDelimiter(basePersonsPath)).build());
+
+		Table baseTrips = Table.read().csv(CsvReadOptions.builder(IOUtils.getBufferedReader(baseTripsPath))
+			.columnTypesPartial(columnTypes)
+			.sample(false)
+			.separator(CsvOptions.detectDelimiter(baseTripsPath)).build());
+
+//		only analyze person agents
+		basePersons = filterPersonAgents(basePersons);
+		persons = filterPersonAgents(persons);
+
+
+//		create labels for dist and income groups
+		List<String> distLabels = getLabels(distGroups);
+		List<String> incomeLabels = getLabels(incomeGroups);
+
+//		add group columns for dist and income
+		addGroupColumn(basePersons, INCOME, incomeGroups, incomeLabels);
+		addGroupColumn(persons, INCOME, incomeGroups, incomeLabels);
+
+//		filter Leipzig agents
+		Table basePersonsLeipzig = filterLeipzigAgents(basePersons);
+		Table personsLeipzig = filterLeipzigAgents(persons);
+
+//		the 2 populations should consist of the same persons
+		if (basePersonsLeipzig.rowCount() != personsLeipzig.rowCount()) {
+			log.fatal("Number of agents living in Leipzig for base ({}) and policy case ({}) are not the same!", basePersonsLeipzig.rowCount(), personsLeipzig.rowCount());
+			throw new IllegalStateException();
+		}
+
+		// Use longest_distance_mode where main_mode is not present
+		trips.stringColumn(MAIN_MODE)
+			.set(trips.stringColumn(MAIN_MODE).isMissing(),
+				trips.stringColumn(LONG_MODE));
+		baseTrips.stringColumn(MAIN_MODE)
+			.set(baseTrips.stringColumn(MAIN_MODE).isMissing(),
+				baseTrips.stringColumn(LONG_MODE));
+
+
+//		calc modal split for base and policy
+		writeModeShare(trips, personsLeipzig, distLabels, "mode_share.csv");
+		writeModeShare(baseTrips, basePersonsLeipzig, distLabels, "mode_share_base.csv");
+
+//		calc modal shift base to policy
+		writeModeShift(trips, baseTrips);
+
+//		join persons and trips
+		Table joined = new DataFrameJoiner(trips, PERSON).inner(persons);
+		Table baseJoined = new DataFrameJoiner(baseTrips, PERSON).inner(basePersons);
+
+//		write income group distr for mode bike in policy and base
+		writeIncomeGroups(joined, incomeLabels, TransportMode.bike, "_income_groups.csv");
+		writeIncomeGroups(baseJoined, incomeLabels, TransportMode.bike, "_income_groups_base.csv");
+
+//		write income group distr for every mode in base (Leipzig)
+		Table baseJoinedLeipzig = new DataFrameJoiner(baseTrips, PERSON).inner(basePersonsLeipzig);
+
+		modes.add("allModes");
+		modes.forEach(m -> writeIncomeGroups(baseJoinedLeipzig, incomeLabels, m, INCOME_SUFFIX));
+
+//		filter for bike trips
+		Table bikeJoined = filterModeAgents(joined, Set.of(TransportMode.bike));
+
+//		filter for trips "cycleHighwayAgents" map
+		IntList idx = new IntArrayList();
+
+		for (int i = 0; i < bikeJoined.rowCount(); i++) {
+			Row row = bikeJoined.row(i);
+
+			int tripStart = durationToSeconds(row.getString("dep_time"));
+//			waiting time already included in travel time
+			int travelTime = durationToSeconds(row.getString(TRAV_TIME));
+
+//			if person is not using bike highway there will be an empty list -> id will not be added to idx list
+			List<Integer> enterTimes = new ArrayList<>(highwayPersons.getOrDefault(row.getString(PERSON), new ArrayList<>()));
+
+			for (int enterTime : enterTimes) {
+				if (Range.of(tripStart, tripStart + travelTime).contains(enterTime)) {
+					idx.add(i);
+				}
+			}
+		}
+//		write trip start and end of every trip using cycle highway to csv
+		bikeJoined = bikeJoined.where(Selection.with(idx.toIntArray()));
+
+//		we need the trip_id column after writing the location to csv, but trip_id should notbe written to csv
+		Table bikeJoinedCsv = bikeJoined.selectColumns(PERSON, "start_x", "start_y", "end_x", "end_y");
+		bikeJoinedCsv.write().csv(output.getPath("cycle_highway_agents_trip_start_end.csv").toFile());
+
+//		filter base trip ids for trip ids of bikeJoined
+		TextColumn tripIdCol = baseJoined.textColumn(TRIP_ID);
+		baseJoined = baseJoined.where(tripIdCol.isIn(bikeJoined.textColumn(TRIP_ID)));
+
+		calcAndWriteMeanStats(bikeJoined, baseJoined);
+
+		writeHighwaysShpFile();
+
+
+//		TODO: figure like plot 4.5 MA thesis?
+		return 0;
+	}
+
+	private void calcAndWriteMeanStats(Table bikeJoined, Table baseJoined) throws IOException {
+		DoubleColumn distanceCol = bikeJoined.doubleColumn(TRAV_DIST);
+
+//		Create int column out of time string values hh:mm:ss
+		IntColumn timeCol = IntColumn.create(TRAV_TIME + "_s", bikeJoined
+			.stringColumn(TRAV_TIME)
+			.asList()
+			.stream()
+			.mapToInt(CycleHighwayAnalysis::durationToSeconds)
+			.toArray());
+
+//		calc mean / median distances / times
+		double meanDist = distanceCol.mean();
+		double medianDist = distanceCol.median();
+		double meanTravTime = timeCol.mean();
+		double medianTravTime = timeCol.median();
+
+		DoubleColumn baseDistanceCol = baseJoined.doubleColumn(TRAV_DIST);
+
+		IntColumn baseTimeCol = IntColumn.create(TRAV_TIME + "_s", baseJoined
+			.stringColumn(TRAV_TIME)
+			.asList()
+			.stream()
+			.mapToInt(CycleHighwayAnalysis::durationToSeconds)
+			.toArray());
+
+		double baseMeanDist = baseDistanceCol.mean();
+		double baseMedianDist = baseDistanceCol.median();
+		double baseMeanTravTime = baseTimeCol.mean();
+		double baseMedianTravTime = baseTimeCol.median();
+
+		//		write mean stats to csv
+		DecimalFormat f = new DecimalFormat("0.00", new DecimalFormatSymbols(Locale.ENGLISH));
+
+		try (CSVPrinter printer = new CSVPrinter(new FileWriter(output.getPath("mean_travel_stats.csv").toString()),
+			CSVFormat.DEFAULT.builder()
+			.setQuote(null)
+			.setDelimiter(',')
+			.setRecordSeparator("\r\n")
+			.build())) {
+			printer.printRecord("\"mean travel distance policy case\"", f.format(meanDist));
+			printer.printRecord("\"mean travel distance base case\"", f.format(baseMeanDist));
+			printer.printRecord("\"median travel distance policy case\"", f.format(medianDist));
+			printer.printRecord("\"median travel distance base case\"", f.format(baseMedianDist));
+			printer.printRecord("\"mean travel time policy case\"", f.format(meanTravTime));
+			printer.printRecord("\"mean travel time base case\"", f.format(baseMeanTravTime));
+			printer.printRecord("\"median travel time policy case\"", f.format(medianTravTime));
+			printer.printRecord("\"median travel time base case\"", f.format(baseMedianTravTime));
+		}
+	}
+
+	private void writeHighwaysShpFile() throws IOException {
+
+		ShapeFileWriter.writeGeometries(new ShpOptions(highwaysShpPath, crs, null).readFeatures(), output.getPath("cycle_highways.shp").toString());
+
+//		We cannot use the same output option for 2 different files, so the string has to be manipulated
+		String prj = output.getPath("cycle_highways.shp").toString().replace(".shp", ".prj");
+
+//		.prj file needs to be simplified to make it readable for simwrapper
+		try (BufferedWriter writer = IOUtils.getBufferedWriter(prj)) {
+			writer.write(crs);
+		}
+	}
+
+	private void writeIncomeGroups(Table joined, List<String> incomeLabels, String mode, String outputFile) {
+
+//		only filter if specific mode is given
+		if (!mode.equals("allModes")){
+			joined = filterModeAgents(joined, Set.of(mode));
+		}
+
+		Table aggr = joined.summarize(TRIP_ID, count).by("income_group");
+
+		DoubleColumn countColumn = aggr.doubleColumn("Count [trip_id]");
+		DoubleColumn share = countColumn.divide(countColumn.sum()).setName("share");
+		aggr.addColumns(share);
+
+		// Sort by income_group
+		Comparator<Row> cmp = Comparator.comparingInt(row -> incomeLabels.indexOf(row.getString("income_group")));
+		aggr = aggr.sortOn(cmp);
+
+		aggr.write().csv(output.getPath(mode + outputFile).toFile());
+	}
+
+	private void writeModeShift(Table trips, Table baseTrips) {
+		Table baseFiltered = filterModeAgents(baseTrips, modes);
+		Table filtered = filterModeAgents(trips, modes);
+
+		baseFiltered.column(MAIN_MODE).setName("original_mode");
+
+		Table joined = new DataFrameJoiner(filtered, TRIP_ID).inner(true, baseFiltered);
+		Table aggr = joined.summarize(TRIP_ID, count).by("original_mode", MAIN_MODE);
+
+		aggr.write().csv(output.getPath("mode_shift.csv").toFile());
+	}
+
+	private void writeModeShare(Table trips, Table persons, List<String> labels, String outputFile) {
+
+//		join needed to filter for Leipzig agents only
+//		filter modes to exclude freight and drt modes
+		Table joined = filterModeAgents(new DataFrameJoiner(trips, PERSON).inner(persons), modes);
+
+		addGroupColumn(joined, TRAV_DIST, distGroups, labels);
+
+		Table aggr = joined.summarize(TRIP_ID, count).by(TRAV_DIST + "_group", MAIN_MODE);
+
+		DoubleColumn share = aggr.numberColumn(2).divide(aggr.numberColumn(2).sum()).setName("share");
+		aggr.replaceColumn(2, share);
+
+		// Sort by dist_group and mode
+		Comparator<Row> cmp = Comparator.comparingInt(row -> labels.indexOf(row.getString(TRAV_DIST + "_group")));
+		aggr = aggr.sortOn(cmp.thenComparing(row -> row.getString(MAIN_MODE)));
+
+		aggr.write().csv(output.getPath(outputFile).toFile());
+
+		// Derive mode order if not given
+		if (modeOrder == null) {
+			modeOrder = new ArrayList<>();
+			for (Row row : aggr) {
+				String mainMode = row.getString(MAIN_MODE);
+				if (!modeOrder.contains(mainMode)) {
+					modeOrder.add(mainMode);
+				}
+			}
+		}
+	}
+
+	private List<String> getLabels(List<Double> groups) {
+		List<String> labels = new ArrayList<>();
+		for (int i = 0; i < groups.size() - 1; i++) {
+			labels.add(String.format("%d - %d", groups.get(i).intValue(), groups.get(i + 1).intValue()));
+		}
+		labels.add(groups.get(groups.size() - 1) + "+");
+		groups.add(Double.MAX_VALUE);
+		return labels;
+	}
+
+	private void addGroupColumn(Table table, String valueLabel, List<Double> groups, List<String> labels) {
+		StringColumn group = table.doubleColumn(valueLabel)
+			.map(dist -> cut(dist, groups, labels), ColumnType.STRING::create).setName(valueLabel + "_group");
+		table.addColumns(group);
+	}
+
+	private static String cut(double value, List<Double> groups, List<String> labels) {
+		int idx = Collections.binarySearch(groups, value);
+
+		if (idx >= 0)
+			return labels.get(idx);
+
+		int ins = -(idx + 1);
+		return labels.get(ins - 1);
+	}
+
+	private Table filterLeipzigAgents(Table persons) {
+		Geometry geometry = shp.getGeometry();
+		GeometryFactory f = new GeometryFactory();
+
+		IntList idx = new IntArrayList();
+		for (int i = 0; i < persons.rowCount(); i++) {
+			Row row = persons.row(i);
+			Point p = f.createPoint(new Coordinate(row.getDouble("first_act_x"), row.getDouble("first_act_y")));
+			if (geometry.contains(p)) {
+				idx.add(i);
+			}
+		}
+		return persons.where(Selection.with(idx.toIntArray()));
+	}
+
+	private Table filterPersonAgents(Table persons) {
+		IntList idx = new IntArrayList();
+		for (int i = 0; i < persons.rowCount(); i++) {
+			Row row = persons.row(i);
+			String subPop = row.getText("subpopulation");
+
+			if (subPop.equals(PERSON)) {
+				idx.add(i);
+			}
+		}
+		return persons.where(Selection.with(idx.toIntArray()));
+	}
+
+	private Table filterModeAgents(Table trips, Set<String> filterModes) {
+		IntList idx = new IntArrayList();
+		for (int i = 0; i < trips.rowCount(); i++) {
+			Row row = trips.row(i);
+			String mainMode = row.getString(MAIN_MODE);
+
+			if (filterModes.contains(mainMode)) {
+				idx.add(i);
+			}
+		}
+		return trips.where(Selection.with(idx.toIntArray()));
+	}
+
+	private static int durationToSeconds(String d) {
+		String[] split = d.split(":");
+		return (Integer.parseInt(split[0]) * 60 * 60) + (Integer.parseInt(split[1]) * 60) + Integer.parseInt(split[2]);
+	}
+
+	private final class CycleHighwayEventHandler implements PersonEntersVehicleEventHandler, LinkEnterEventHandler {
+
+		@Override
+		public void handleEvent(PersonEntersVehicleEvent event) {
+//			register personId with vehId to get personId in LinkEnterEvent
+			if (event.getVehicleId().toString().contains(TransportMode.bike)) {
+				bikers.putIfAbsent(event.getVehicleId(), event.getPersonId().toString());
+			}
+		}
+
+		@Override
+		public void handleEvent(LinkEnterEvent event) {
+			if (event.getLinkId().toString().contains(LINK_PREFIX)) {
+				if (!highwayPersons.containsKey(bikers.get(event.getVehicleId()))) {
+					highwayPersons.put(bikers.get(event.getVehicleId()), new ArrayList<>());
+				}
+				highwayPersons.get(bikers.get(event.getVehicleId())).add((int) event.getTime());
+			}
+		}
+	}
+}
---
+++ src/main/java/org/matsim/dashboard/CycleHighwayDashboard.java
@@ -0,0 +1,182 @@
+package org.matsim.dashboard;
+
+import org.matsim.analysis.CycleHighwayAnalysis;
+import org.matsim.application.analysis.traffic.TrafficAnalysis;
+import org.matsim.application.prepare.network.CreateGeoJsonNetwork;
+import org.matsim.simwrapper.Dashboard;
+import org.matsim.simwrapper.Header;
+import org.matsim.simwrapper.Layout;
+import org.matsim.simwrapper.viz.*;
+import tech.tablesaw.plotly.traces.BarTrace;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Shows information about an optional policy case, which implements cycle highways in Leipzig.
+ * It also compares the agents and their trips using the cycle highways with their respective trips in the base case.
+ */
+public class CycleHighwayDashboard implements Dashboard {
+	private final String basePath;
+	private final String shp;
+	private final String highwaysShp;
+	private static final String SHARE = "share";
+	private static final String ABSOLUTE = "Count [trip_id]";
+	private static final String INCOME_GROUP = "income_group";
+	private static final String CRS = "EPSG:25832";
+	private static final String SOURCE = "source";
+	private static final String MAIN_MODE = "main_mode";
+	private static final String TRAFFIC = "traffic";
+
+	CycleHighwayDashboard(String basePath, String shp, String highwaysShp) {
+		if (!basePath.endsWith("/")) {
+			basePath += "/";
+		}
+		this.basePath = basePath;
+		this.shp = shp;
+		this.highwaysShp = highwaysShp;
+	}
+
+	@Override
+	public void configure(Header header, Layout layout) {
+		header.title = "Cycle Highways Dashboard";
+		header.description = "Shows statistics about agents, who used the newly implemented cycle highway " +
+			"and compares to the corresponding trips in the base case.";
+
+		String[] args = new ArrayList<>(List.of("--base-path", basePath, "--shp", shp, "--highways-shp-path", highwaysShp)).toArray(new String[0]);
+
+		layout.row("first")
+			.el(Tile.class, (viz, data) -> {
+				viz.dataset = data.compute(CycleHighwayAnalysis.class, "mean_travel_stats.csv", args);
+				viz.height = 0.1;
+			});
+
+		layout.row("modalSplit")
+			.el(Plotly.class, (viz, data) -> {
+				viz.title = "Modal split";
+
+				viz.layout = tech.tablesaw.plotly.components.Layout.builder()
+					.barMode(tech.tablesaw.plotly.components.Layout.BarMode.STACK)
+					.build();
+
+				Plotly.DataSet ds = viz.addDataset(data.compute(CycleHighwayAnalysis.class, "mode_share.csv", args))
+					.constant(SOURCE, "Policy")
+					.aggregate(List.of(MAIN_MODE), SHARE, Plotly.AggrFunc.SUM);
+
+				Plotly.DataSet dsBase = viz.addDataset(data.compute(CycleHighwayAnalysis.class, "mode_share_base.csv", args))
+					.constant(SOURCE, "Base")
+					.aggregate(List.of(MAIN_MODE), SHARE, Plotly.AggrFunc.SUM);
+
+				viz.mergeDatasets = true;
+
+				viz.addTrace(BarTrace.builder(Plotly.OBJ_INPUT, Plotly.INPUT).orientation(BarTrace.Orientation.HORIZONTAL).build(),
+					ds.mapping()
+						.name(MAIN_MODE)
+						.y(SOURCE)
+						.x(SHARE)
+				);
+				viz.addTrace(BarTrace.builder(Plotly.OBJ_INPUT, Plotly.INPUT).orientation(BarTrace.Orientation.HORIZONTAL).build(),
+					dsBase.mapping()
+						.name(MAIN_MODE)
+						.y(SOURCE)
+						.x(SHARE)
+				);
+			})
+			.el(Sankey.class, (viz, data) -> {
+				viz.title = "Mode shift (to bike)";
+				viz.width = 1.5d;
+				viz.description = "by main mode. Compares base case output with output after the last iteration";
+				viz.csv = data.compute(CycleHighwayAnalysis.class, "mode_shift.csv", args);
+			});
+
+		layout.row("locations")
+			.el(Hexagons.class, (viz, data) -> {
+
+				viz.title = "Cycle highway trips - Origins";
+				viz.center = data.context().getCenter();
+				viz.zoom = data.context().mapZoomLevel;
+				viz.height = 7.5;
+				viz.width = 2.0;
+				viz.file = data.compute(CycleHighwayAnalysis.class, "cycle_highway_agents_trip_start_end.csv");
+				viz.projection = CRS;
+				viz.addAggregation("trip origins", "person", "start_x", "start_y");
+			})
+			.el(Hexagons.class, (viz, data) -> {
+				viz.title = "Cycle highway trips - Destinations";
+				viz.center = data.context().getCenter();
+				viz.zoom = data.context().mapZoomLevel;
+				viz.height = 7.5;
+				viz.width = 2.0;
+				viz.file = data.compute(CycleHighwayAnalysis.class, "cycle_highway_agents_trip_start_end.csv");
+				viz.projection = CRS;
+				viz.addAggregation("trip destinations", "person", "end_x", "end_y");
+			})
+			.el(MapPlot.class, (viz, data) -> {
+				viz.title = "Cycle highways";
+				viz.center = data.context().getCenter();
+				viz.zoom = data.context().mapZoomLevel;
+				viz.height = 7.5;
+				viz.width = 2.0;
+				viz.setShape(data.compute(CycleHighwayAnalysis.class, "cycle_highways.shp"), "id");
+			});
+
+		layout.row("volumes")
+			.el(MapPlot.class, (viz, data) -> {
+
+				viz.title = "Simulated traffic volume by bike";
+				viz.center = data.context().getCenter();
+				viz.zoom = data.context().mapZoomLevel;
+				viz.height = 7.5;
+				viz.width = 2.0;
+				viz.setShape(data.compute(CreateGeoJsonNetwork.class, "network.geojson", "--with-properties", "--mode-filter", "car,freight,drt,bike"), "id");
+				viz.addDataset(TRAFFIC, data.compute(TrafficAnalysis.class, "traffic_stats_by_link_daily.csv", "--transport-modes" , "car,bike,freight"));
+
+				viz.display.lineColor.dataset = TRAFFIC;
+				viz.display.lineColor.columnName = "vol_bike";
+				viz.display.lineColor.join = "link_id";
+				viz.display.lineColor.setColorRamp(ColorScheme.RdYlBu, 5, true);
+
+				viz.display.lineWidth.dataset = TRAFFIC;
+				viz.display.lineWidth.columnName = "vol_bike";
+				viz.display.lineWidth.scaleFactor = 20000d;
+				viz.display.lineWidth.join = "link_id";
+
+			});
+
+		createIncomeLayouts(layout, args);
+
+	}
+
+	private static void createIncomeLayouts(Layout layout, String[] args) {
+		Layout.Row row1 = layout.row("income");
+		createBarChart(row1, "Bike users per income group - Policy", "bike_income_groups.csv", args, SHARE, List.of(SHARE));
+		createBarChart(row1, "Bike users per income group - Policy", "bike_income_groups.csv", args, ABSOLUTE, List.of(ABSOLUTE));
+
+		Layout.Row row2 = layout.row("incomeBase");
+		createBarChart(row2, "Bike users per income group - Base", "bike_income_groups_base.csv", args, SHARE, List.of(SHARE));
+		createBarChart(row2, "Bike users per income group - Base", "bike_income_groups_base.csv", args, ABSOLUTE, List.of(ABSOLUTE));
+
+		Layout.Row row3 = layout.row("incomeBaseLeipzig");
+		createBarChart(row3, "Leipzig residents per income group - Base", "allModes_income_groups_base_leipzig.csv", args, SHARE, List.of(SHARE));
+		createBarChart(row3, "Leipzig residents (bike) per income group - Base", "bike_income_groups_base_leipzig.csv", args, SHARE, List.of(SHARE));
+		createBarChart(row3, "Leipzig residents (car) per income group - Base", "car_income_groups_base_leipzig.csv", args, SHARE, List.of(SHARE));
+
+		Layout.Row row4 = layout.row("incomeBaseLeipzig2");
+		createBarChart(row4, "Leipzig residents (pt) per income group - Base", "pt_income_groups_base_leipzig.csv", args, SHARE, List.of(SHARE));
+		createBarChart(row4, "Leipzig residents (walk) per income group - Base", "walk_income_groups_base_leipzig.csv", args, SHARE, List.of(SHARE));
+		createBarChart(row4, "Leipzig residents (ride) per income group - Base", "ride_income_groups_base_leipzig.csv", args, SHARE, List.of(SHARE));
+	}
+
+	private static Layout.Row createBarChart(Layout.Row row, String title, String fileName, String[] args, String yAxisName, List<String> columns) {
+
+		return row.el(Bar.class, (viz, data) -> {
+			viz.title = title;
+			viz.stacked = false;
+			viz.dataset = data.compute(CycleHighwayAnalysis.class, fileName, args);
+			viz.x = INCOME_GROUP;
+			viz.xAxisName = INCOME_GROUP;
+			viz.yAxisName = yAxisName;
+			viz.columns = columns;
+		});
+	}
+}
